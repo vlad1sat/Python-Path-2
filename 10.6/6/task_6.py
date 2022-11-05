21c8baec94ef9daa6e974a81ad2c2f2658903717
vlad1sat <pyatkov200316@mail.ru>
2022-11-05 14:54:34 +0500

    create mini-chat

--- 10.6/6/task_6.py
+++ 10.6/6/task_6.py
@@ -1,7 +1,20 @@
 name_user = input("Введите свой никнейм: ")
 
-with open('chat.txt', 'a', encoding='utf-8') as chat:
-    while True:
-        command = input("\nВведите команду!\n1. Посмотреть текущий текст чата.\n2. Отправить сообщение.\n")
+while True:
+    try:
+        command = int(
+            input("\nВведите команду!\n1. Посмотреть текущий текст чата.\n2. Отправить сообщение.\n3. Выход.\n"))
+    except ValueError:
+        print("Неверно введенная команда!")
+    else:
         if command == 1:
-            print(chat)
+            with open('chat.txt', 'r', encoding='utf-8') as show_chat:
+                for chat_message in show_chat:
+                    print(chat_message, end='')
+        elif command == 2:
+            with open('chat.txt', 'a', encoding='utf-8') as add_chat:
+                message = input("Введите сообщение: ")
+                add_chat.write(''.join([name_user, ': ', message, '\n']))
+        elif command == 3:
+            print("Выход из чата!")
+            break
