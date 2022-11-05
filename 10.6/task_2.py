ca27e7304819bd480c0c145a9bc2a7614be7835e
vlad1sat <pyatkov200316@mail.ru>
2022-11-05 15:02:06 +0500

    refactor 10.6.2

--- 10.6/task_2.py
+++ 10.6/task_2.py
@@ -15,14 +15,14 @@ def f2(x, y):
 
 with open('coordinates.txt', 'r') as file:
     with open('result.txt', 'w') as file_2:
-        try:
-            for line in file:
-                nums_list = line.split()
+        for line in file:
+            nums_list = line.split()
+            number = random.randint(0, 100)
+            try:
                 res1 = f(int(nums_list[0]), int(nums_list[1]))
                 res2 = f2(int(nums_list[0]), int(nums_list[1]))
-                number = random.randint(0, 100)
                 my_list = sorted([res1, res2, number])
                 file_2.write(' '.join(my_list))
-        except Exception:
-            print("Что-то пошло не так!")
+            except Exception:
+                print("Что-то пошло не так с методами!")
 
