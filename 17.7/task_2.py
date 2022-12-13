42259e5ac4118c064f230c9c9313fedfc3d9a028
vlad1sat <pyatkov200316@mail.ru>
2022-12-13 21:25:49 +0500

    refactor 17.7

--- 17.7/task_2.py
+++ 17.7/task_2.py
@@ -4,5 +4,5 @@ letters: List[str] = ['a', 'b', 'c', 'd', 'e']
 numbers: List[int] = [1, 2, 3, 4, 5, 6, 7, 8]
 
 if __name__ == '__main__':
-    my_zip = list(map(lambda letter, number: (letter, number), letters, numbers))
+    my_zip: List[(str, int)] = list(map(lambda letter, number: (letter, number), letters, numbers))
     print(my_zip)
